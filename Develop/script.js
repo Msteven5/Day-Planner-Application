49c0cf62f21658626a60e3c6b2cf567b222a0266
Msteven <msdavid55@gmail.com>
2024-01-06 02:05:54 -0600

    js file updated/application meets minimum requirements

--- Develop/script.js
+++ Develop/script.js
@@ -5,54 +5,69 @@ var currentDate = dayjs().format('dddd, MMMM d')
 $('#currentDay').text(currentDate)
 
 var currentHour = dayjs().format('HH')
-var currentMinute = dayjs().format(' : mm A')
+var currentMinute = dayjs().format(' : mm')
 $('#currentTime').text(currentHour + currentMinute)
 
-
-// var divEle = document.getElementById("9")
-// console.log(divEle)
-
 let timeSlot = []
+let divInfo = []
+let btnInfo = []
+let textInfo = []
 
-for( i = 9; i <= 17; i++) {
+for(let i = 9; i <= 17; i++) {
   let times = document.getElementById(i);
   timeSlot.push(times);
+  divInfo.push(times.children);
 }
 
 
-$(function () {
-  for( i = 0; i < timeSlot.length;) {
-    if( timeSlot[i] == currentHour) {
-      timeSlot[i].toggleClass("past future")
+for(let i = 0; i < divInfo.length; i++) { 
+    btns = divInfo[i][2];
+    text = divInfo[i][1];
+    btnInfo.push(btns);
+    textInfo.push(text);
+  }
+  
+for(let i = 0; i < textInfo.length;) {
+     let eventOnScreen = localStorage.getItem("event-saved " + i);
+      let realEventOnScreen = JSON.parse(eventOnScreen);
+        if (realEventOnScreen !== null) {
+          $(textInfo[i]).text(realEventOnScreen);
+          i++;
+          } else {
+            i++;
+            continue;
+          }
+   }
+
+
+for(let i = 0; i < timeSlot.length; i++) {
+  if (parseInt(timeSlot[i].id) === currentHour) {
+    timeSlot[i].classList.add("present");
+  } else if (
+    parseInt(timeSlot[i].id) > currentHour) {
+      timeSlot[i].classList.add("future");
+    } else {
+    parseInt(timeSlot[i].id) < currentHour; {
+      timeSlot[i].classList.add("past");
     }
-  
-} 
-  
-  
-  
- 
- //jquery has .toggleClass to be used later
- //change hour class tags in html to just numbers and use for loop to compare it to the currentHour
-  
-  
-  
-  
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+  }
+}
+
+
+console.log(timeSlot);
+console.log(divInfo);
+console.log(btnInfo);
+console.log(textInfo);
+
+for(let i = 0; i < btnInfo.length; i++) {
+ btnInfo[i].addEventListener("click", function() {
+  if (textInfo[i]) {
+    let event = JSON.stringify(textInfo[i].value);
+      localStorage.setItem("event-saved " + i, event);
+  }
+ })
+}
+
+// $(function () {
+//   }
+// })
